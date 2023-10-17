1f59d73dc8130baf5a56134c6b7dfaf92ca930b6
BENZ buddharuksa <ebuddharuksa@sfsu.edu>
2023-10-16 19:37:46 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# This code is responsible for tracking fish in a given area. It uses computer vision techniques to detect and track fish movements in real-time. The code is part of a larger project called "fish-tracking" which aims to provide researchers with a tool to study fish behavior and migration patterns.
+
+# Project Name
+
+Fish-Tracking
+
+# Description
+
+
+# Installation
+
+To install Fish-Tracking, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Install the required dependencies using pip.
+
+
+# Usage
+
+
+# Contributing
+
+If you would like to contribute to Fish-Tracking, please follow these guidelines:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them to your branch.
+4. Push your changes to your fork.
+5. Submit a pull request to the main repository.
+
---
+++ video_count.py
@@ -0,0 +1,154 @@
+import cv2
+import os
+import sys
+if len(sys.argv) != 2:
+    print("Error: Invalid arguments.")
+    print("Usage: python video_count.py tracker_type")
+    print("Needed one argument: tracker_type:Number ")
+    print("The tracker type should be an integer in the range [0, 6].")
+    print("0: BOOSTING, 1: MIL, 2: KCF, 3: TLD, 4: MEDIANFLOW, 5: CSRT, 6: MOSSE")
+    exit()
+
+tracker_type = int(sys.argv[1])
+if tracker_type < 0 or tracker_type > 6:
+    print("Error: Invalid tracker type.")
+    print("The tracker type should be an integer in the range [0, 6].")
+    print("0: BOOSTING, 1: MIL, 2: KCF, 3: TLD, 4: MEDIANFLOW, 5: CSRT, 6: MOSSE")
+    exit()
+tracker_type_name = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'CSRT', 'MOSSE', 'DaSiamRPN', 'GOTURN', 'CSRT_NONLEGACY']
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+relative_path = 'labels'
+label_folder = os.path.join(absolute_path, relative_path)
+
+def create_tracker(tracker_type):
+    if tracker_type == 0:
+        return cv2.legacy.TrackerBoosting.create()
+    if tracker_type == 1:
+        return cv2.legacy.TrackerMIL.create()
+    if tracker_type == 2:
+        return cv2.legacy.TrackerKCF.create()
+    if tracker_type == 3:
+        return cv2.legacy.TrackerTLD.create()
+    if tracker_type == 4:
+        return cv2.legacy.TrackerMedianFlow.create()
+    if tracker_type == 5:
+        return cv2.legacy.TrackerCSRT.create()
+    if tracker_type == 6:
+        return cv2.legacy.TrackerMOSSE.create()
+    """
+    if tracker_type == 7:
+        return cv2.TrackerDaSiamRPN.create()
+    if tracker_type == 8:
+        return cv2.TrackerGOTURN.create()
+    if tracker_type == 9:
+        return cv2.TrackerCSRT.create()
+    """
+
+def convert_to_absolute(coords, frame_shape, factor=0.7):
+    _, x_center, y_center, width, height = map(float, coords)
+
+    width *= factor
+    height *= factor
+
+    x_center_abs = int(x_center * frame_shape[1])
+    y_center_abs = int(y_center * frame_shape[0])
+    width_abs = int(width * frame_shape[1])
+    height_abs = int(height * frame_shape[0])
+
+    x1 = int(x_center_abs - (width_abs / 2))
+    y1 = int(y_center_abs - (height_abs / 2))
+
+    return x1, y1, width_abs, height_abs
+
+
+def is_overlapping(rect1, rect2):
+    x1_1, y1_1, w1, h1 = rect1
+    x2_1, y2_1, w2, h2 = rect2
+
+    x1_2, y1_2 = x1_1 + w1, y1_1 + h1
+    x2_2, y2_2 = x2_1 + w2, y2_1 + h2
+
+    return not (x1_2 < x2_1 or x1_1 > x2_2 or y1_2 < y2_1 or y1_1 > y2_2)
+
+
+
+# load labels from file to array
+label = [label for label in os.listdir(label_folder)]
+label.sort()
+label_container = []    
+for la in label:
+    with open(os.path.join(label_folder, la), 'r') as f:
+        line = f.read().splitlines()
+        line_ac = [float(val) for sublist in line for val in sublist.split()]
+        label_container.append(line_ac)
+    
+# get first label and remove it from the container
+
+
+
+
+video = cv2.VideoCapture('video1.mp4')
+if not video.isOpened():
+    print("Error: Could not open video.")
+    exit()
+
+# Read first frame
+ret, frame = video.read()
+label_container_abs = [convert_to_absolute(coords, frame.shape) for coords in label_container]
+first_location = label_container_abs.pop(0)
+
+# Create a tracker object
+tracker = create_tracker(tracker_type)
+
+# Initialize the tracker with the calculated bounding box
+tracker.init(frame, first_location) 
+
+
+
+count_no_overlap = 0
+count_tracking_failed = 0
+
+
+for i in range(len(label_container_abs)):
+    ret, frame = video.read()
+    if not ret:
+        break
+    
+    success, box = tracker.update(frame)
+    if success:
+        (x, y, w, h) = [int(v) for v in box]
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, f'{tracker_type_name[tracker_type]} Tracker', (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+        
+        cv2.rectangle(frame, (label_container_abs[i][0],label_container_abs[i][1]), (label_container_abs[i][0]+label_container_abs[i][2], label_container_abs[i][1]+label_container_abs[i][3]), (255, 0, 0), 2)
+        cv2.putText(frame, f'Real Fish Location', (label_container_abs[i][0], label_container_abs[i][1]-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+        if not is_overlapping((x, y, w, h), label_container_abs[i]):
+            count_no_overlap = count_no_overlap + 1
+            # Delete the existing tracker and create a new one
+            tracker = create_tracker(tracker_type)
+            x_new, y_new, w_new, h_new = label_container_abs[i]
+            
+            # Initialize the tracker with the new bounding box
+            tracker.init(frame, (x_new, y_new, w_new, h_new))
+    else:
+        count_tracking_failed = count_tracking_failed + 1
+        # Delete the existing tracker and create a new one
+        tracker = create_tracker(tracker_type)
+        x_new, y_new, w_new, h_new = label_container_abs[i]
+        
+        # Initialize the tracker with the new bounding box
+        tracker.init(frame, (x_new, y_new, w_new, h_new))
+    
+    cv2.putText(frame, f'No Overlap Count: {count_no_overlap}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    cv2.putText(frame, f'Tracking Failed Count {count_tracking_failed}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+    cv2.putText(frame, f'Tracker Type: {tracker_type_name[tracker_type]}', (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+    frame_resized = cv2.resize(frame, (1600,1024))
+
+    cv2.imshow(tracker_type_name[tracker_type], frame_resized)
+    if cv2.waitKey(30) & 0xFF == 27:  # Press 'Esc' to exit
+        break
+cv2.destroyAllWindows()
+
+print(f'No Overlap Count: {count_no_overlap}')
+print(f'Tracking Failed Count {count_tracking_failed}')
+
